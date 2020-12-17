7b1cb1414dde9e4ddc4cf93efc95b8410bc66632
Maria Riazanova <mv_13@mail.ru>
2020-12-17 21:17:01 +0300

    feat: add map

--- index.js
+++ index.js
@@ -1,51 +1,83 @@
 import './src/css/style.css';
-import Data from './src/js/Data';
-import Header from './src/js/Header';
+import L from 'leaflet';
+// import Data from './src/js/Data';
+
+
 import Map from './src/js/Map';
 import Table from './src/js/Table';
 import Chart from './src/js/Chart';
 
-const map = new Map(51.505, -0.09, 2);
+const map = new Map(51.505, -0.09, 2, true);
 map.createMap();
 
-Data.getAPIData();
 
+
+
+// console.log(obj);
+/*
+getData().then(result => {
+    console.log(result);
+    return result;
+    }
+)
+*/
+
+
+// Data.getCountriesData();
+// Data.getAPIData();
+
+//Data2.getData();
+//Data2.getInfoData();
+
+/*
+
+async function getData(){
+    let stat = await fetch('https://api.covid19api.com/summary');
+    let stat2 = await fetch('https://restcountries.eu/rest/v2/all');
+    let com = await stat.json();
+    let com2 = await stat2.json();
+    return { com, com2 };
+}
+
+
+getData().then(result => {
+    console.log(result);
+    result.com.Countries.forEach(element => {
+        console.log('1');
+        const country = result.com2.find(item => item.alpha2Code === element.CountryCode);
+        console.log(country);
+     //   console.log(element.name, element.alpha2Code, element.latlng);
+     //   console.log(result.com.Countries.find(item => item.CountryCode === element.alpha2Code));
+        if (element.CountryCode === country.alpha2Code ) {
+            console.log(element.Country, element.CountryCode, country.latlng);
+            const circleCenter = country.latlng;
+            console.log(circleCenter);
+            const circleOptions = {
+                color: 'red',
+                fill: 'true',
+                fillColor: '#f03',
+                fillOpacity: 0.5
+            };
+            const circleRadius = element.TotalConfirmed;
+            console.log(element.TotalConfirmed);
+            console.log(circleRadius);
+
+            const circle = L.circle(circleCenter, circleRadius, circleOptions);
+            circle.bindPopup(`${element.Country}\n${element.TotalConfined}`);
+            circle.addTo(map);
+            console.log('circle');
+        }
+    });
+})
+    
+
+*/
 /*
 import cors from 'cors';
 
 const server = express();
 server.use(cors());
 */
-/*
-fetch('https://api.covid19api.com/summary')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
-*/
-  /*
-  const { data = [] } = response;
-const hasData = Array.isArray(data) && data.length > 0;
 
-// if ( !hasData ) return;
 
-const geoJson = {
-  type: 'FeatureCollection',
-  features: data.map((country = {}) => {
-    const { countryInfo = {} } = country;
-    const { lat, long: lng } = countryInfo;
-    return {
-      type: 'Feature',
-      properties: {
-       ...country,
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [ lng, lat ]
-      }
-    }
-  })
-}
-*/
+
--- src/js/Map.js
+++ src/js/Map.js
@@ -1,27 +1,102 @@
 import L from 'leaflet';
+// import regeneratorRuntime from "regenerator-runtime";
+import Data from './Data';
+// import dataz from './DataFunction';
+
 
 class Map{
 
-    constructor(x, y, zoom){
+    constructor(x, y, zoom, worldCopyJump){
         this.x = x;
         this.y = y;
         this.zoom = zoom;
+        this.worldCopyJump = worldCopyJump;
     }
 
-    createMap(){
+    createMap() {
         const mapDiv = document.createElement('div');
         mapDiv.setAttribute('id','map');        
         document.querySelector('body').append(mapDiv);
 
-        // Creating a map object
-        const map = new L.Map('map').setView ([this.x, this.y], this.zoom);
-
+        const map = new L.Map('map', {
+             center: [this.x, this.y], 
+             zoom: this.zoom, 
+             worldCopyJump: this.worldCopyJump
+            }
+        );
+        
         // Creating a Layer object
         const layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
         // Adding layer to the map
         map.addLayer(layer);
+
+
+        async function getData(){
+            let infoCOVID = await fetch('https://api.covid19api.com/summary');
+            let infoCountries = await fetch('https://restcountries.eu/rest/v2/all');
+            let resultCOVID = await infoCOVID.json();
+            let resultCountries = await infoCountries.json();
+            return { resultCOVID, resultCountries };
+        }
+        
+        
+        getData().then(result => {
+            console.log(result);
+            result.resultCOVID.Countries.forEach(element => {
+                console.log('1');
+                const country = result.resultCountries.find(item => item.alpha2Code === element.CountryCode);
+                console.log(country);
+          
+                if (element.CountryCode === country.alpha2Code ) {
+                    console.log(element.Country, element.CountryCode, country.latlng);
+                    const circleCenter = country.latlng;
+                    console.log(circleCenter);
+                    const circleOptions = {
+                        color: 'red',
+                        fill: 'true',
+                        fillColor: 'red',
+                        fillOpacity: 0.5
+                    };
+                    const circleRadius = element.TotalConfirmed / 10;
+                    console.log(element.TotalConfirmed);
+                    console.log(circleRadius);
+        
+                    const circle = L.circle(circleCenter, circleRadius, circleOptions);
+                    circle.bindPopup(`${element.Country}<br> TotalConfirmed: ${element.TotalConfirmed}`);
+                    circle.addTo(map);
+                    console.log('circle');
+                }
+            });
+        })
+
+        this.createLegend(map);
+  
     }
+
+    createLegend(map) {
+       // const map = document.getElementById('map');
+        const legend = L.control({position: 'bottomright'});
+
+        legend.onAdd = function (map) {
+        
+            let div = L.DomUtil.create('div', 'info legend'),
+                grades = ['Total confirmed cases', '2'],
+                labels = ["src/images/ballRed.png","src/images/ballRed.png"];
+        
+            // loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                div.innerHTML +=
+                    grades[i] + (" <img src="+ labels[i] +" height='20' width='20'>") +'<br>';
+            }
+        
+            return div;
+        };
+        
+        legend.addTo(map);
+
+    }
+
 
 }
 
