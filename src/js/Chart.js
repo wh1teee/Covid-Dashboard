a847033dc5a5ab3e8abaf3647a17aab9b7b8bc4c
Danil Kohan <Sportodavka@gmail.com>
2020-12-22 20:33:53 +0300

    feat: created a chart

--- src/js/Chart.js
+++ src/js/Chart.js
@@ -1,5 +1,31 @@
+import Chart from "chart.js";
 class Chart {
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        document.querySelector("body").appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+    }
 
+    createChart(x, y) {
+      this.chart = new Chart(this.ctx, {
+          type: "bar",
+          data: {
+              labels: x,
+              datasets: [
+                  {
+                      label: "number of cases",
+                      data: y,
+                      backgroundColor: "yellow",
+                  },
+              ],
+          },
+      })   
+      const chart = new Chart()
+      chart.createChart(
+          ["January","February","March","April","May","June","July","August","September","October","November","December"],
+          [50, 100, 200, 300, 400, 500, 550, 350, 480, 500, 600, 150]
+      )
+    
 
 
 
