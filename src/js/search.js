c896eadae2c91ac41cfe50a89e6f23c6b64c9e02
wh1teee <hemp0@mail.ru>
2020-12-23 21:17:23 +0300

    feat: add block/unblock function buttons ( case/name )

--- src/js/search.js
+++ src/js/search.js
@@ -1,13 +1,12 @@
-
 export default function inputListen() {
     const input = document.querySelector('.input')
-    input.oninput = function() {
+    input.oninput = function () {
         const val = this.value.trim()
         const elasticItems = document.querySelectorAll('.stat__item')
 
         if (val !== '') {
             elasticItems.forEach(item => {
-                if(item.innerText.toLowerCase().search(val) === -1) {
+                if (item.innerText.toLowerCase().search(val) === -1) {
                     item.classList.add('hide')
                 } else {
                     item.classList.remove('hide')
@@ -18,4 +17,16 @@ export default function inputListen() {
         }
     }
 
+}
+
+export function blockOrUnblockButtons(length) {
+    const casesSortBtn = document.querySelector('.cases__sort')
+    const namesSortBtn = document.querySelector('.name__sort')
+    if (length > 0) {
+        casesSortBtn.disabled = true
+        namesSortBtn.disabled = true
+    } else {
+        casesSortBtn.disabled = false
+        namesSortBtn.disabled = false
+    }
 }
